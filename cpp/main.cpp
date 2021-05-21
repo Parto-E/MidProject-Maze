7795c0f67db418f3aab618ed80023a8bce280505
Parto-E <Parto-E@users.noreply.github.com>
2021-05-21 19:55:07 +0430

    second commit: The end of maze is not in right place

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -13,6 +13,9 @@ using std::vector;
 enum DIR { NORTH, SOUTH, EAST, WEST };
 
 int main(void){
+   //check how many cells visitted
+   int visittedcells{1};
+
    // Randomize the random number function.
    srand(time(NULL));
 
@@ -26,7 +29,15 @@ int main(void){
 
    // For each Cell in the maze:
    for(int row = 0; row < ROWS; row++)
-      for(int col = 0; col < COLS; col++) { 
+      for(int col = 0; col < COLS; col++) {
+         //set first cell visitted
+         if(row == 0 && col == 0){
+            maze[row][col].setVisited(true);
+            // set its position to its row and column in the maze
+            maze[row][col].setPosition(row, col);
+            // set the Cell's walls to Cell::WALL_ALL
+            maze[row][col].setWalls(Cell::WALL_ALL);
+         }
          // set visited to false
          maze[row][col].setVisited(false);
          // set its position to its row and column in the maze
@@ -48,66 +59,79 @@ int main(void){
    // Grab the Cell at the curX, curY position and push it on the trail stack.
    trail.push_back(maze[curX][curY]);
 
-   // While the trail stack is not empty do the following:
-   while(trail.empty()==false) { // stay in here till display
-      // Empty the live vector.
-      live.clear();
-      // Check the neighbors of the current cell to the north, east, south, and west.
-      // If any of the neighbors have all four walls, add the direction to that 
-      // neighbor to the live vector.
-      if(curY)
-         if(maze[curX][curY-1].getWalls()==Cell::WALL_ALL) // West has all walls
-            live.push_back(WEST);
-      if(curY<COLS-1)
-      if(maze[curX][curY+1].getWalls()==Cell::WALL_ALL) // east has all walls
-         live.push_back(EAST);
-      if(curX)
-      if(maze[curX-1][curY].getWalls()==Cell::WALL_ALL) // North has all walls
-         live.push_back(NORTH);
-      if(curX<ROWS-1)
-      if(maze[curX+1][curY].getWalls()==Cell::WALL_ALL) // South has all walls
-         live.push_back(SOUTH);
-      // If the live vector is not empty:
-      if(live.empty()==false) {
-         // Choose one of the directions in the live vector at random
-         // Remove the walls between the current cell and the neighbor in that direction
-         // and Change curX and curY to refer to the neighbor
-         maze[curX][curY].setVisited(true);
-         switch(live[rand() % live.size()]) {
-            case 0:
-               maze[curX][curY].removeWall(Cell::WALL_NORTH);
-               maze[--curX][curY].removeWall(Cell::WALL_SOUTH);
-               break;
-            case 1:
-               maze[curX][curY].removeWall(Cell::WALL_SOUTH);
-               maze[++curX][curY].removeWall(Cell::WALL_NORTH);
-               break;
-            case 2:
-               maze[curX][curY].removeWall(Cell::WALL_EAST);
-               maze[curX][++curY].removeWall(Cell::WALL_WEST);
-               break;
-            case 3:
-               maze[curX][curY].removeWall(Cell::WALL_WEST);
-               maze[curX][--curY].removeWall(Cell::WALL_EAST);
-               break;
-         }
-         // Push the new current cell onto the trail stack
-         /*cout << "Maze " << curX << ", " << curY << "\n";
-         cin.ignore(); */
-         trail.push_back(maze[curX][curY]);
-      } //If the live vector was emtpy:
-      else {
-         // Pop the top item from the trail stack
-         trail.pop_back();
 
-         // If the trail stack is not empty, set curX and curY to refer to the 
-         // position of the top item in the trail stack.
-         if(trail.empty()==false) {
-            curX=trail[0].getRow();
-            curY=trail[0].getColumn();
+      // While the trail stack is not empty do the following:
+      while(trail.empty()==false) { // stay in here till display
+         // if we did not reach all cells do the following:
+         if(visittedcells < ROWS*COLS){
+         // Empty the live vector.
+         live.clear();
+         // Check the neighbors of the current cell to the north, east, south, and west.
+         // If any of the neighbors have all four walls, add the direction to that 
+         // neighbor to the live vector.
+         if(curY)
+            if(maze[curX][curY-1].getWalls()==Cell::WALL_ALL) // West has all walls
+               live.push_back(WEST);
+         if(curY<COLS-1)
+            if(maze[curX][curY+1].getWalls()==Cell::WALL_ALL) // east has all walls
+               live.push_back(EAST);
+         if(curX)
+            if(maze[curX-1][curY].getWalls()==Cell::WALL_ALL) // North has all walls
+               live.push_back(NORTH);
+         if(curX<ROWS-1)
+            if(maze[curX+1][curY].getWalls()==Cell::WALL_ALL) // South has all walls
+               live.push_back(SOUTH);
+         // If the live vector is not empty:
+         if(!live.empty()) {
+            // Choose one of the directions in the live vector at random
+            // Remove the walls between the current cell and the neighbor in that direction
+            // and Change curX and curY to refer to the neighbor
+            maze[curX][curY].setVisited(true);
+            switch(live[rand() % live.size()]) {
+               //NORTH
+               case 0:
+                  maze[curX][curY].removeWall(Cell::WALL_NORTH);
+                  maze[--curX][curY].removeWall(Cell::WALL_SOUTH);
+                  trail.push_back(maze[curX][curY - 1]);
+                  break;
+               //SOUTH
+               case 1:
+                  maze[curX][curY].removeWall(Cell::WALL_SOUTH);
+                  maze[++curX][curY].removeWall(Cell::WALL_NORTH);
+                  trail.push_back(maze[curX][curY + 1]);
+                  break;
+               //EAST
+               case 2:
+                  maze[curX][curY].removeWall(Cell::WALL_EAST);
+                  maze[curX][++curY].removeWall(Cell::WALL_WEST);
+                  trail.push_back(maze[curX + 1][curY]);
+                  break;
+               //WEST
+               case 3:
+                  maze[curX][curY].removeWall(Cell::WALL_WEST);
+                  maze[curX][--curY].removeWall(Cell::WALL_EAST);
+                  trail.push_back(maze[curX - 1][curY]);
+                  break;
+            }
+            // Push the new current cell onto the trail stack
+            //trail.push_back(maze[curX][curY]);
+            maze[curX][curY].visited();
+            visittedcells++;
+         } //If the live vector was emtpy:
+         else {
+            // Pop the top item from the trail stack
+            trail.pop_back();
+
+            // If the trail stack is not empty, set curX and curY to refer to the 
+            // position of the top item in the trail stack.
+            if(trail.empty()==false) {
+               curX=trail[0].getRow();
+               curY=trail[0].getColumn();
+            }
          }
       }
    }
+
 
    // Do the following to display the maze:
     int r, c;
