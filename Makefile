1abff590c74cc88affa5e4230d9d9261afbe8290
Parto-E <Parto-E@users.noreply.github.com>
2021-05-27 15:54:42 +0430

    Fifth commit: using nested classes for better looking code

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
 CXX = g++
 CXXFLAGS = -std=c++2a -Wall -I h -I /usr/local/include/gtest/ -c
 LXXFLAGS = -std=c++2a -Ih -pthread
-OBJECTS = ./obj/main.o ./obj/cell.o
+OBJECTS = ./obj/main.o ./obj/maze.o
 TARGET = main
 
 
 $(TARGET): $(OBJECTS)
 	$(CXX) $(LXXFLAGS) -o $(TARGET) $(OBJECTS) $(GTEST)
-./obj/main.o: ./cpp/main.cpp ./h/cell.h ./cpp/
+./obj/main.o: ./cpp/main.cpp ./h/maze.h
 	$(CXX) $(CXXFLAGS) ./cpp/main.cpp -o ./obj/main.o
-./obj/cell.o: ./cpp/cell.cpp
-	$(CXX) $(CXXFLAGS) ./cpp/cell.cpp -o ./obj/cell.o
+./obj/maze.o: ./cpp/maze.cpp
+	$(CXX) $(CXXFLAGS) ./cpp/maze.cpp -o ./obj/maze.o
 clean:
 	rm -fv $(TARGET) $(OBJECTS)
---
+++ h/maze.h
@@ -0,0 +1,48 @@
+#ifndef __MAZE_H_
+#define __MAZE_H_
+
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
+
+class Maze{
+    public:
+        class Cell {
+        private:
+            int row;
+            int col;
+            bool visit;
+            int walls;
+            void init(const int r, const int c, const int walls, const bool v = false);
+        public:
+            enum WALL { WALL_NORTH = 0x0008, WALL_EAST = 0x0004,
+                WALL_SOUTH  = 0x0002, WALL_WEST = 0x0001,
+                WALL_ALL = 0x000f, WALL_NONE = 0x0000 };
+            Cell();
+            Cell(const int r, const int c);
+            Cell(const int r, const int c, const int stat);
+            bool visited() const;
+            void setVisited(const bool v = true);
+            int getRow() const;
+            int getColumn() const;
+            void removeWall(const int w);
+            int getWalls() const;
+            void setWalls(const int w);
+            void setPosition(const int r, const int c);
+            friend std::ostream& operator<<(std::ostream& strm, const Cell& c);
+        };
+
+
+
+        Maze(int ROWS, int COLS);
+        void start();
+
+    private:
+        int ROWS;
+        int COLS;
+        enum DIR { NORTH, SOUTH, EAST, WEST };
+};
+
+#endif 
